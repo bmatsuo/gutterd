8cdcd0021520789aa87473b82dbc058c14f4847e
Bryan Matsuo <bryan.matsuo@gmail.com>
2012-03-11 17:04:54 -0700

    Add web server stub files.

---
+++ web.go
@@ -0,0 +1,18 @@
+// Copyright 2012, Bryan Matsuo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+/*  Filename:    web.go
+ *  Author:      Bryan Matsuo <bmatsuo@soe.ucsc.edu>
+ *  Created:     2012-03-11 16:24:28.171009 -0700 PDT
+ *  Description: 
+ */
+
+import (
+	//"net/http"
+)
+
+func ListenAndServe() {
+}
---
+++ web_test.go
@@ -0,0 +1,17 @@
+package main
+
+/*  Filename:    web_test.go
+ *  Author:      Bryan Matsuo <bmatsuo@soe.ucsc.edu>
+ *  Created:     2012-03-11 16:24:28.171262 -0700 PDT
+ *  Description: For testing web.go
+ */
+
+import (
+    "testing"
+)
+
+
+func TestWeb(t *testing.T) {
+
+}
+
