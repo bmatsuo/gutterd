6c39dc6ff8ca86461c50456bd9c29f65d56d9dff
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-05-28 12:58:17 -0700

    add metadata test and test torrent files

--- metadata/metadata_test.go
+++ metadata/metadata_test.go
@@ -7,11 +7,29 @@ package metadata
  */
 
 import (
-    "testing"
+	"os"
+	"path/filepath"
+	"testing"
 )
 
-
-func TestMetadata(t *testing.T) {
-
+func TestReadMetadataFile(t *testing.T) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("failed to determine working directory: %v", err)
+	}
+	t.Logf("pwd: %v", cwd)
+	testfiles, err := filepath.Glob(filepath.Join(cwd, "test", "*.torrent"))
+	if err != nil {
+		t.Fatalf("failed to find test torrent files: %v", err)
+	}
+	for _, filename := range testfiles {
+		meta, err := ReadMetadataFile(filename)
+		if err != nil {
+			t.Errorf("failed to read file: %v", err)
+			continue
+		}
+		if meta.Announce == "" {
+			t.Errorf("no announce url for file %q", filename)
+		}
+	}
 }
-
